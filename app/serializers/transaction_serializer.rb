4a3fa00f1dd83a9ab16cf41f9d783c82993d83c2
github username <github email address>
2017-10-03 15:04:10 -0400

    verify transactions

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,4 +1,4 @@
 class TransactionSerializer < ActiveModel::Serializer
-	attributes :id, :contacter, :post, :status, :hours, :verified, :message
+	attributes :id, :contacter, :post, :status, :hours, :verified, :message, :hours_logged
 	belongs_to :post
 end
