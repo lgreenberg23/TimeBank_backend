921aa8a6518e6f0c5b3b361ee6fa15e7928f6eb3
github username <github email address>
2018-02-15 11:13:03 -0500

    cors configuration

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module TimeBankBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*',
